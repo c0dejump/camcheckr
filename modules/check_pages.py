825a11c4d53e29337c629641a5694a1709f1414f
c0dejump <codejumpgame@gmail.com>
2020-12-21 15:12:36 +0100

    Update check_pages.py

--- modules/check_pages.py
+++ modules/check_pages.py
@@ -1,17 +1,19 @@
 import requests
-from references import basic_link
-from config import PLUS, INFO, LINE
+from config import PLUS, INFO, LINE, REDI
 from requests.exceptions import Timeout
 
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
 
 def check_pages(url, s):
 	print("\n{} Testing known pages of cameras:\n".format(INFO))
-	for pages in basic_link:
-		url_page = "{}{}".format(url, pages) if url[-1] == "/" else "{}/{}".format(url, pages)
-		try:
-			res = requests.get(url_page, verify=False, timeout=3, allow_redirects=True)
-		except Exception:
-			traceback.print_exc()
-		if res.status_code not in [404, 403, 401, 503]:
-			print("{}[{}]{}".format(PLUS, res.status_code, url_page))
+	with open("urls_cam.txt", "r+") as basic_link:
+		for pages in basic_link.readlines():
+			url_page = "{}{}".format(url, pages.strip()) if url[-1] == "/" else "{}/{}".format(url, pages.strip())
+			try:
+				res = requests.get(url_page, verify=False, timeout=6, allow_redirects=False)
+			except Exception:
+				traceback.print_exc()
+			if res.status_code in [301, 302]:
+				print("{}[{}] {}".format(REDI, res.status_code, url_page))
+			elif res.status_code not in [404, 403, 401, 503]:
+				print("{}[{}] {}".format(PLUS, res.status_code, url_page))
